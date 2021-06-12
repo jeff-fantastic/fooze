18de6110bf609d670bfd4ac29795b42b6648b913
Jeffrey Fantastic <48529067+jefftasticgames@users.noreply.github.com>
2021-06-12 02:48:06 -0400

    Baaalls in ya jaws

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,18 +1,21 @@
 extends Area2D
 
+export (int) var ID = 0
+
 var ON = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if get_overlapping_bodies() == []:
-		ON = false
-		$Box.color = Color(1,1,1,1)
+func _process(_delta):
+	if ON:
+		$vis.color = Color(1,0,0,1)
 	else:
+		$vis.color = Color(1,1,1,1)
+
+func _on_button_body_entered(body):
+	if body in KinematicBody2D or RigidBody2D:
 		ON = true
-		$Box.color = Color(1,0,0,1)
+
+func _on_button_body_exited(body):
+	if body in KinematicBody2D or RigidBody2D:
+		if get_overlapping_bodies() == []:
+			ON = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -10,9 +10,15 @@ func _physics_process(_delta):
 	#print(GOO_AMOUNT)
 	$col.scale = Vector2(clamp(lerp($col.scale.x, GOO_AMOUNT / 50, 0.05), 0.2, 2), clamp(lerp($col.scale.y, GOO_AMOUNT / 50, 0.05), 0.2, 2))
 	$vis.scale = Vector2(clamp(lerp($vis.scale.x, GOO_AMOUNT / 50, 0.05), 0.2, 2), clamp(lerp($vis.scale.y, GOO_AMOUNT / 50, 0.05), 0.2, 2))
+	$camera.zoom = Vector2(clamp(lerp($camera.zoom.x, GOO_AMOUNT / 25, 0.01), 1, 3), clamp(lerp($camera.zoom.y, GOO_AMOUNT / 25, 0.01), 1, 3))
 	$spawn_gimbal.scale = Vector2(clamp(lerp($spawn_gimbal.scale.x, GOO_AMOUNT / 50, 0.05), 0.2, 2), clamp(lerp($spawn_gimbal.scale.y, GOO_AMOUNT / 50, 0.05), 0.2, 2))
 	$spawn_gimbal.look_at(get_global_mouse_position())
 	mass = clamp(GOO_AMOUNT / 50, 0.5, 1.5)
+	if $sfx.get_child_count() > 0: # sound cleanup
+		var children = $sfx.get_children()
+		for i in $sfx.get_child_count():
+			if !children[i].playing:
+				children[i].queue_free()
 	move()
 
 func move():
@@ -35,5 +41,11 @@ func spawn_mass():
 func _on_detector_body_entered(body):
 	if body.is_in_group("mass"):
 		if body.ENABLED:
+			var slurp = AudioStreamPlayer2D.new()
+			slurp.stream = load("res://sfx/slurp.wav")
+			slurp.pitch_scale = rand_range(0.7, 1.2)
+			slurp.volume_db = rand_range(-15, -5)
+			slurp.play()
+			$sfx.add_child(slurp)
 			GOO_AMOUNT += body.TARGET_SCALE * 10
 			body.queue_free()
